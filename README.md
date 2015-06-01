4e80b769a381e5b78a89d086506caabb09e0fe52
Ondrej Brinkel <info@anzui.de>
2015-06-01 11:30:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ES6 Promise.series
+
+Execute promises in series
+
+```javascript
+Promise.series([
+ aPromise,
+ anotherPromise
+])
+  .then(function (results) {
+    ...
+  });
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = function(Promise)
+{
+  Promise.series = function(promises) {
+    var promises = promises.slice();
+    var results = [];
+    var first = true;
+
+    return new Promise(function(resolve, reject) {
+      function next(result)
+      {
+        if (!first) {
+          results.push(result);
+        }
+        else {
+          first = false;
+        }
+
+        if (promises.length) {
+          var promise = promises.shift();
+
+          promise
+          .then(next)
+          .catch(reject);
+        }
+        else {
+          resolve(results);
+        }
+      }
+
+      next();
+    });
+  }
+};
+

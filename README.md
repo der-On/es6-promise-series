1fc5959cfa136b82d2390dd7e7963273e3985552
Ondrej Brinkel <info@anzui.de>
2015-11-23 16:50:48 +0100

    v0.1.0 - added concurrency option - added tests - added travis badge

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
+[![Build Status](https://travis-ci.org/der-On/es6-promise-series.svg)](https://travis-ci.org/der-On/es6-promise-series)
+
 # ES6 Promise.series
 
 Execute promises in series
 
 ```javascript
+/**
+ * Runs promises in series
+ * @param  {Array} list of promises or functions returning promises
+ * @param  {Number} (optional) number of concurrent promises, of omitted no concurrency will happen
+ * @return {Promise}
+ */
 Promise.series([
  aPromise,
  anotherPromise
-])
+], 4)
   .then(function (results) {
     ...
   });
--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 "use strict";
 
-module.exports = function(Promise)
-{
-  Promise.series = function(promises) {
-    var promises = promises.slice();
-    var results = [];
-    var first = true;
+module.exports = function(Promise) {
+  Promise.series = function (promises, concurrent) {
+    var results = null;
+    promises = promises.slice();
+    concurrent = concurrent || 1;
 
-    return new Promise(function(resolve, reject) {
-      function next(result)
-      {
-        if (!first) {
-          results.push(result);
-        }
-        else {
-          first = false;
+    return new Promise(function (resolve, reject) {
+      function next(result) {
+        console.log('next', promises.length, (new Date()).getTime());
+        var concurrentPromises = [];
+        var promise;
+
+        if (!results) {
+          results = [];
+        } else {
+          results = results.concat(result);
         }
 
         if (promises.length) {
-          var promise = promises.shift();
+          while (concurrentPromises.length < concurrent && promises.length) {
+            var promise = promises.shift();
 
-          if (!promise || typeof promise.then !== 'function') {
-            promise = Promise.resolve();
+            if (typeof promise === 'function') {
+              promise = promise();
+            }
+
+            if (!promise || typeof promise.then !== 'function') {
+              promise = Promise.resolve();
+            }
+
+            concurrentPromises.push(promise);
           }
 
-          promise
-          .then(next)
-          .catch(reject);
-        }
-        else {
+          Promise.all(concurrentPromises)
+            .then(next)
+            .catch(reject);
+        } else {
           resolve(results);
         }
       }
 
       next();
     });
-  }
+  };
 };
